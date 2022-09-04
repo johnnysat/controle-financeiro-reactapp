7dee567c0fd1dc97a93216ff8beace71081901ea
Johnny <johnny_s.o@hotmail.com>
2022-09-04 15:17:19 -0300

    Versão Web Finalizada

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as C from "./styles";
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Haeader>
+        <C.Title>
+          Controle Financeiro
+        </C.Title>
+      </C.Haeader>
+    </C.Container>
+  );
+}
+
+export default Header;
